f0fe04024790500739ab8e93df4e6b2a4e89e9ae
Josué Cesar <mendonca.josue@gmail.com>
2021-06-06 23:48:12 -0300

    feat: cards has been created, must be editable in the next feature

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import HighlightCard from '../../components/HighlightCard';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/59187401?v=4' }} />
+
+            <User>
+              <UserGreeting>Olá</UserGreeting>
+              <UserName>Josué</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
     </Container>
   );
 }
